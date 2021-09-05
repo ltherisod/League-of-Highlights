119ecd9e908de0d54339e245a41cf59cd729ac5e
Cristian Tortoza <tortozacris@gmail.com>
2021-09-04 23:52:26 -0300

    check

--- config/passport.js
+++ config/passport.js
@@ -1,14 +1,15 @@
 // const passport = require('passport')
-
-// const passport = require('passport')
 // const jwtStrategy = require('passport-jwt').Strategy 
 // const extractJwt = require('passport-jwt').ExtractJwt
+// const User = require('../models/User')
 
 // module.exports = passport.use(
 // 	new jwtStrategy({
 // 		jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 // 		secretOrKey: process.env.SECRETKEY
 // 	},
-	
+// 	(payload, done)=>{
+
+// 	}
 // 	)
 // )
