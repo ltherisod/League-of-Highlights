f936af29260ad15c33807fa0cccbd284f9f704e9
Cristian Tortoza <tortozacris@gmail.com>
2021-09-04 23:52:52 -0300

    nada

--- config/passport.js
+++ config/passport.js
@@ -1,15 +1,23 @@
-// const passport = require('passport')
-// const jwtStrategy = require('passport-jwt').Strategy 
-// const extractJwt = require('passport-jwt').ExtractJwt
-// const User = require('../models/User')
+const passport = require('passport')
+const jwtStrategy = require('passport-jwt').Strategy 
+const extractJwt = require('passport-jwt').ExtractJwt
+const User = require('../models/User')
 
-// module.exports = passport.use(
-// 	new jwtStrategy({
-// 		jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-// 		secretOrKey: process.env.SECRETKEY
-// 	},
-// 	(payload, done)=>{
-
-// 	}
-// 	)
-// )
+module.exports = passport.use(
+	new jwtStrategy({
+		jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+		secretOrKey: process.env.SECRETKEY
+	},
+	(payload, done)=>{
+		User.findOne({_id : payload._doc._id})
+		.then(respuesta => {
+			if(!respuesta){
+				return done(null, false)
+			}else{
+				return done(null, respuesta)
+			}
+		})
+		.catch((error) => done(error, false))
+	}
+	)
+)
