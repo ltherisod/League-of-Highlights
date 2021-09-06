bc418db8e2c4974fed121ad71a467e8d1cf7180b
ejercicio-01 <nicole.dorola@gmail.com>
2021-09-06 16:48:16 -0300

    home

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,25 +3,25 @@ import "./Footer.css"
 
 const Footer = () => {
     return (
-        <footer className="divFooter" style={{backgroundImage: "url('https://i.postimg.cc/R0Mn1bWd/footerok.png')"}}>
+        <footer className="divFooter" style={{backgroundImage: "url('https://i.postimg.cc/zvCK05xy/footer2.png')"}}>
             <div className="footerBox">
                 <div className="logo">
                     <img className="logoFooter" src="./assets/LOHWhite.png"/>
                 </div>
                 <div className="socialMedia">
-                    <NavLink to="https://www.youtube.com/user/riotgamesinc">
+                    <NavLink className="socialMediaHover" to="https://www.youtube.com/user/riotgamesinc">
                         <img className="socialMediaIcon" src="./assets/youtube.png"/>
                     </NavLink>
-                    <NavLink to="https://twitter.com/leagueoflegends">
+                    <NavLink className="socialMediaHover" to="https://twitter.com/leagueoflegends">
                         <img className="socialMediaIcon" src="./assets/twitter.png"/>
                     </NavLink>
-                    <NavLink to="https://www.facebook.com/leagueoflegends">
+                    <NavLink className="socialMediaHover" to="https://www.facebook.com/leagueoflegends">
                         <img className="socialMediaIcon" src="./assets/facebook.png"/>
                     </NavLink>
-                    <NavLink to="https://www.instagram.com/leagueoflegends/">
+                    <NavLink className="socialMediaHover" to="https://www.instagram.com/leagueoflegends/">
                         <img className="socialMediaIcon" src="./assets/instagram.png"/>
                     </NavLink> 
-                    <NavLink to="https://www.reddit.com/r/leagueoflegends/">
+                    <NavLink className="socialMediaHover" to="https://www.reddit.com/r/leagueoflegends/">
                         <img className="socialMediaIcon" src="./assets/reddit.png"/>
                     </NavLink> 
                 </div>
--- frontend/src/components/News.js
+++ frontend/src/components/News.js
@@ -1,13 +1,55 @@
 import "./News.css"
+import Carousel from "react-bootstrap/Carousel"
+import { NavLink } from "react-router-dom"
+
+let rotations = [
+    "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Fiora_0.jpg",
+    "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Gwen_0.jpg",
+    "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Lux_0.jpg",
+    "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Kaisa_0.jpg",
+    "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Seraphine_0.jpg",
+    "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Nunu_0.jpg"
+]
 
 const News = () => {
     return (
-        <div className="news" style={{backgroundImage: "url('https://i.postimg.cc/qq6dMMDz/background-Image.png')"}}>
+        <div className="news">
+            <div className="socialContent">
+                <p>JOIN OUR SOCIAL NETWORKS |</p>
+                <div className="socialBox">
+                    <NavLink className="socialMediaHover" to="https://www.youtube.com/user/riotgamesinc">
+                        <img className="socialMediaIcon" src="./assets/youtube.png"/>
+                    </NavLink>
+                    <NavLink className="socialMediaHover" to="https://twitter.com/leagueoflegends">
+                        <img className="socialMediaIcon" src="./assets/twitter.png"/>
+                    </NavLink>
+                    <NavLink className="socialMediaHover" to="https://www.facebook.com/leagueoflegends">
+                        <img className="socialMediaIcon" src="./assets/facebook.png"/>
+                    </NavLink>
+                    <NavLink className="socialMediaHover" to="https://www.instagram.com/leagueoflegends/">
+                        <img className="socialMediaIcon" src="./assets/instagram.png"/>
+                    </NavLink> 
+                    <NavLink className="socialMediaHover" to="https://www.reddit.com/r/leagueoflegends/">
+                        <img className="socialMediaIcon" src="./assets/reddit.png"/>
+                    </NavLink> 
+                </div>
+            </div>
             <h2>News</h2>
             <div className="rotationsContainer">
-                <div className="rotation"></div>
-                <div className="rotation"></div>
-                <div className="rotation"></div>
+                <Carousel className="rotationsCarousel">
+                    {rotations.map((rotation, index) => {
+                        return(
+                            <Carousel.Item key={index}>
+                            <div className="rotationImg" style={{backgroundImage: `url('${rotation}')`}}>    
+                            </div>
+                            <Carousel.Caption>
+                            <h4 className="rotationTitle">titulo rotacion</h4>
+                            </Carousel.Caption>
+                            </Carousel.Item>
+                        )
+                    })
+                    }
+                </Carousel> 
             </div>
         </div>
     )
