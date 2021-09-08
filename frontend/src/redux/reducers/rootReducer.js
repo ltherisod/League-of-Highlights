69a17aa69769268828e3ebf582360a1617fa04cc
unknown <lucre.gillone@gmail.com>
2021-09-08 17:57:20 -0300

    render carousel

--- frontend/src/redux/reducers/rootReducer.js
+++ frontend/src/redux/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux'
 import userReducer from './userReducer'
+import videosReducer from "./videosReducer"
 
 const rootReducer = combineReducers({
-	user: userReducer
+	user: userReducer, 
+	video: videosReducer
 })
 
 export default rootReducer
--- frontend/src/redux/reducers/videosReducer.js
+++ frontend/src/redux/reducers/videosReducer.js
@@ -1,4 +1,4 @@
-const userReducer = (state = { userVideos: [] }, action) => {
+const videosReducer = (state = { userVideos: [] }, action) => {
 	switch (action.type) {
 		case "GET_VIDEOS_USER":
 			return {...state, userVideos: action.payload}
