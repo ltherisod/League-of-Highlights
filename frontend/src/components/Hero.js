6ed0692e168d2fb4c8661bb70038f3906460d9a6
ltherisod <lauratherisod@gmail.com>
2021-09-07 22:17:27 -0300

    cambios

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <>
         <video loop autoPlay muted className="video">
-          <source src="/assets/video.mp4" type="video/mp4" />
+          <source src="/assets/videoplayback.mp4" type="video/mp4" />
           </video>
           <div className="contentVideo">
           <Jumbotron className='d-flex justify-content-center align-items-center flex-column'>
