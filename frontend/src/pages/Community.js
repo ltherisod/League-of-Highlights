06deeeb8c92b22836928c03b50194c17508b2014
ltherisod <lauratherisod@gmail.com>
2021-09-09 00:25:06 -0300

    touches

--- frontend/src/pages/Community.js
+++ frontend/src/pages/Community.js
@@ -25,7 +25,7 @@ const Community = () => {
               <div className="picCommunity" style={{backgroundImage:"url('https://i.postimg.cc/0yx5NHVL/Ee-Q6x-P-XYAAe-Yhq.jpg')"}}></div>
               <div className="picCommunity" style={{backgroundImage:"url('https://i.postimg.cc/Njy91n6L/FHOp4q6oq-RYUl3m-X2-X9-ZPpc-Pr-Z3vki-EAqivaf-Xj-RGc-E-1.jpg')"}}></div>
             </div>
-            <div className="picBox" data-aos="fade-left">
+            <div className="picBox" data-aos="fade-right">
               <div className="picCommunity" style={{backgroundImage:"url('https://i.postimg.cc/L8yDsGHX/DUlz7f-XXc-AAKse-B.jpg')"}}></div>
               <div className="picCommunity" style={{backgroundImage:"url('https://i.postimg.cc/hGy5HfJ2/de651pd-55834d28-12b3-4edb-be3f-4adf37846837.jpg')"}}></div>
             </div>
