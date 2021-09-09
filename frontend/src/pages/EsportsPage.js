6746a379eef9d5db7df58cbc4c62ef809991c102
ltherisod <lauratherisod@gmail.com>
2021-09-09 12:01:32 -0300

    mediaqueries

--- frontend/src/pages/EsportsPage.js
+++ frontend/src/pages/EsportsPage.js
@@ -12,7 +12,7 @@ const EsportsPage = (props) => {
             <div className="bodyBackground" style={{backgroundImage:"url('https://i.postimg.cc/4dGhW9qH/background-Violeta.png')"}}>
                 <h2 className= "titleEsports animate__animated animate__fadeInUp">ESPORTS</h2>
                 <div className="animate__animated animate__fadeInUp  videoContainer" style={{backgroundImage: "url('https://i.postimg.cc/vmmZW0Cc/nubescall.png')"}}>
-                <ReactPlayer url='http://www.youtube.com/watch?v=avzfp6RQLYA' className="videoEsports" controls={true}/>
+                <ReactPlayer  url='http://www.youtube.com/watch?v=avzfp6RQLYA' className="videoEsports" controls={true}/>
                 <ReactPlayer url='http://www.youtube.com/watch?v=b1Qi_aHsZn0' className="videoEsports" controls={true}/>
                 <ReactPlayer url='http://www.youtube.com/watch?v=53EjdlmdfNc' className="videoEsports" controls={true}/>
                 <ReactPlayer url='http://www.youtube.com/watch?v=wdZ8fVVaL90' className="videoEsports" controls={true}/>
