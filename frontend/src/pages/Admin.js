b9b195579b00645640dae95fd3c90096b8c65cde
Cristian Tortoza <tortozacris@gmail.com>
2021-09-09 18:15:03 -0300

    asd

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -1,20 +1,35 @@
 import "./Admin.css"
 import Header from "../components/Header"
+import { connect } from "react-redux"
+import videosActions from "../redux/actions/videosActions"
 
-const Admin = () => {
+const Admin = (props) => {
+      
+    const deleteReportedVideo = () => {
+        props.deleteVideo() // id del video a borrar.
+    }
+
+    const deleteUser = () => {
+        
+    }
     return (
         <>
             <Header/>
-            <div className="van">
-                <p>":username" was reported :x times</p>
-                <button>van account</button>
-            </div>
-            <div className="deleteVideo">
-                <p>:video reported</p>
-                <button>delete</button>
+            <div className="containerAdmin">
+             <div className="van">
+                    <p>":username" was reported :x times</p>
+                    <button onClick={deleteUser}>van account</button>
+                </div>
+                <div className="deleteVideo">
+                    <p>:video reported</p>
+                    <button onClick={deleteReportedVideo}>delete</button>
+                </div>
             </div>
         </>
     )
 }
+const mapDispatchToProps = {
+    deleteVideo: videosActions.deleteVideo
+}
 
-export default Admin
+export default connect(null, mapDispatchToProps)(Admin)
