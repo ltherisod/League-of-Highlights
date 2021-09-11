1c49b5584a97fa07d2452cc76454873eb266d3cc
ejercicio-01 <nicole.dorola@gmail.com>
2021-09-11 15:00:28 -0300

    home

--- frontend/src/components/CallToAction.js
+++ frontend/src/components/CallToAction.js
@@ -5,11 +5,11 @@ import ReactPlayer from "react-player"
 const CallToAction = () => {
     return (
         <div className="mainCallToAction" style={{backgroundImage: "url('https://i.postimg.cc/vmmZW0Cc/nubescall.png')"}}>
-            <ReactPlayer url='https://www.youtube.com/watch?v=SQjxq7r4R1s' className="videoCallToAction" playing={true} loop={true} volume={0} muted={true} controls={false}/>
-            <div className="buttonAndRock">
-                <Link to="/esports"><button className="buttonCall">PRO VIEWS</button></Link>
-                <img className="rock" src="https://i.postimg.cc/HWb8Whmg/piedra.png" alt=""/>
-            </div>
+            {/* <ReactPlayer url='https://www.youtube.com/watch?v=SQjxq7r4R1s' className="videoCallToAction" playing={true} loop={true} volume={0} muted={true} controls={false}/> */}
+                <div className="callImage" style={{backgroundImage:"url('https://i.postimg.cc/jSmqs2bf/call-Image.png')"}}>
+                    <Link to="/esports"><button className="buttonCall">PRO VIEWS</button></Link>
+                </div>
+                {/* <img className="rock" src="https://i.postimg.cc/HWb8Whmg/piedra.png" alt=""/> */}
         </div>
     )
 }
